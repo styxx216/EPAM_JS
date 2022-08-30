ccba61a7e7129257cb939cc1a3d7dd10568ca123
styxx216 <38997882+styxx216@users.noreply.github.com>
2022-08-30 14:39:38 +0300

    Update task_2_objects_and_functions.js

--- task_2_objects_and_functions.js
+++ task_2_objects_and_functions.js
@@ -1,3 +1,11 @@
+// Create a car object, add a color property to it with the value equals 'black'
+// Change the color property of the car object to 'green'
+// Add the power property to the car object, which is a function and displays the engine power to the console
+// Pears and apples are accepted to the warehouse, write a function that returns the result of adding the number of accepted pears and apples
+// Your name is saved in the payment terminal, write a function to define the name in the terminal (if you entered your name, then hello + name, if not, then there is no such name)
+// Write a function for calculating the type of argument and type output to the console
+// Write a function that determines whether a number is prime or not
+
 let car = Object();
 car.color = 'black';
 car.color = 'green';
