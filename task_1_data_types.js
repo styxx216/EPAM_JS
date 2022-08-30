6ac9cf6ddb31756be1800ba0e170cbcdea62818a
styxx216 <ko4etova.elena.n@gmail.com>
2022-08-30 17:28:54 +0300

    task 1

--- task_1_data_types.js
+++ task_1_data_types.js
@@ -1,7 +1,7 @@
-// Perform addition of various types (string + boolean, string + number, number + boolean)
-// Perform multiplication of various types (string * boolean, string * number, number * boolean)
-// Divide different types (string / boolean, string / number, number / Boolean)
-// Perform explicit conversion (number, string, boolean)
+// 1 Perform addition of various types (string + boolean, string + number, number + boolean)
+// 2 Perform multiplication of various types (string * boolean, string * number, number * boolean)
+// 3 Divide different types (string / boolean, string / number, number / Boolean)
+// 4 Perform explicit conversion (number, string, boolean)
 
 let str = "Hello";
 let num = 1;
