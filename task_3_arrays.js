40bcaf5622613cee3e3d0b8427a8b08f10a2fddb
styxx216 <38997882+styxx216@users.noreply.github.com>
2022-08-30 14:40:29 +0300

    Update task_3_arrays.js

--- task_3_arrays.js
+++ task_3_arrays.js
@@ -1,3 +1,14 @@
+// Given an array consisting of movie names, iterate over the array with the output of the names of each movie to the console
+// Given an array of car manufacturers, convert the array to a string and back to an array
+// Given an array of the names of your friends, add the words hello to each element of the array
+// Convert numeric array to Boolean
+// Sort the array [1,6,7,8,3,4,5,6] in descending order
+// Filter array [1,6,7,8,3,4,5,6] by value> 3
+// Write a function that takes two parameters - an array and a number and outputs the index of an array element equal to a number
+// Implement a loop that will print the number 'a' until it is less than 10
+// Implement a loop that prints prime numbers to the console
+// Implement a loop that prints odd numbers to the console
+
 let movies =['In the Name of the King 2: Two Worlds','Bloodrayne: The Third Reich','1968. Tunnel Rats','Anger of the Dead'];
 let auto = ['Audi','Volkswagen','Honda','Subaru'];
 let names = ['Bob','Alise','Jonh'];
