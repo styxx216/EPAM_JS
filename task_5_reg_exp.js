6e7db7c40926cd7236a18d0fa292f8fbcf1bcee4
styxx216 <ko4etova.elena.n@gmail.com>
2022-09-01 18:57:31 +0300

    task 5

--- task_5_reg_exp.js
+++ task_5_reg_exp.js
@@ -1,7 +1,6 @@
 // Given the string 'ahb acb aeb aeeb adcb axeb'. Write a regular expression that matches the strings ahb, acb, aeb by pattern: letter 'a', any character, letter 'b'
 // Given the string '2 + 3 223 2223'. Write a regex that finds line 2 + 3 without capturing the rest
 
-
 let str_1 = 'ahb acb aeb aeeb adcb axeb';
 let str_2 = '2 + 3 223 2223';
 let reg_1 =/a.b/g
