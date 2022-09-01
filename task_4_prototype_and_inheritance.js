00a72a0ff2cb04f9a823aa21c5366d32c0e62602
styxx216 <ko4etova.elena.n@gmail.com>
2022-09-01 18:55:18 +0300

    task 4

--- task_4_prototype_and_inheritance.js
+++ task_4_prototype_and_inheritance.js
@@ -1,5 +1,6 @@
 // Create 2 objects: animal and cat, add move property to animal object, cat object must inherit move property from object animal
 
+
 class Animal{
     constructor(move){
         this.move = move;
